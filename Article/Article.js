ec2fdd2969647a854b64fb927db46732acb84115
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-01-30 17:34:17 -0800

    Added a new article using index file and created a new class in article.less file

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,15 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-     this.expandButton.innerHTML = "EXPAND";
+     this.expandButton.innerHTML = 'EXPAND';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.expandButton.classList.toggle('expandButton');
+    //this.expandButton.classList.toggle('expandButton');
     this.domElement.classList.toggle('article-open');
-    console.log(this.expandArticle);
+    //console.log(this.expandArticle);
   }
 }
 
